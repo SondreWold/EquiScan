aeb4c315700aa1daff565ac0c70972bcdf44368e
Sondre Wold <sondrewold1@gmail.com>
2024-04-10 14:30:56 +0200

    Test run file

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+lr = 3e-4
+hidden_size = 128
+layers = 2
+dropout = 0.1
+heads = 2
+
+for p in 1 2 4 8 16 32 64
+do
+    sbatch submit.slurm --log --task "simple" --lr $lr --hidden_size $hidden_size --layers $layers --dropout $dropout --heads $heads
+done
+
+sbatch submit.slurm --log --task "length" --lr $lr --hidden_size $hidden_size --layers $layers --dropout $dropout --heads $heads
